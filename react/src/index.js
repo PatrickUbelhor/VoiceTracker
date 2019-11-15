86b9fb046253a89a834ca475fa439c8a776bb9df
PatrickUbelhor <pubelhor@gmail.com>
2019-11-15 13:30:10 -0500

    Added basic React files

---
+++ react/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+// Render the component
+ReactDOM.render(
+<App/>,
+	document.querySelector('#root')
+);
