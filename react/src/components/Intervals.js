d952a753dc887458e056a27c2b5684c854da3b80
PatrickUbelhor <pubelhor@gmail.com>
2019-12-20 22:11:09 -0600

    Added tooltips to intervals

--- react/src/components/Intervals.js
+++ react/src/components/Intervals.js
@@ -1,5 +1,6 @@
 import '../css/Intervals.css';
 import React from 'react';
+import Tooltip from '@material-ui/core/Tooltip';
 
 function Intervals(props) {
 
@@ -7,17 +8,21 @@ function Intervals(props) {
 
 	let boxes = [];
 	for (let i = 0; i < props.intervals.length; i++) {
-		let length = (props.intervals[i].end - props.intervals[i].start) / LENGTH_OF_DAY;
+		let duration = props.intervals[i].end - props.intervals[i].start;
+		let width = duration / LENGTH_OF_DAY;
 		let leftMargin = (i === 0)
 			? props.intervals[0].start / LENGTH_OF_DAY
 			: (props.intervals[i].start - props.intervals[i - 1].end) / LENGTH_OF_DAY;
 
 		let box = (
-			<div key={props.intervals[i].start} className="Bar" style={{
-				marginLeft: 100 * leftMargin + '%',
-				width: 100 * length + '%',
-				backgroundColor: props.color
-			}} />
+			// TODO: Maybe make custom tooltip that doesn't take forever to load?
+			<Tooltip arrow title={Math.floor(duration / 60) + "h " + (duration % 60) + "m"}>
+				<div key={props.intervals[i].start} className="Bar" style={{
+					marginLeft: 100 * leftMargin + '%',
+					width: 100 * width + '%',
+					backgroundColor: props.color
+				}} />
+			</Tooltip>
 		);
 
 		boxes.push(box);
