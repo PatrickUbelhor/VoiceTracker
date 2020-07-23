479d81d0e340cfc6107b532346749bf71437d8d7
Patrick Ubelhor <pubelhor@gmail.com>
2020-07-22 21:24:24 -0500

    Added time markers to day

--- react/src/components/Day.js
+++ react/src/components/Day.js
@@ -16,11 +16,11 @@ function Day(props) {
 	let otherIntervals = [];
 	for (let i = 0; i < users.length; i++) {
 		if (users[i].intervals.length > 0) {
-			let intervalDiv = <Intervals key={i} color={users[i].color} intervals={users[i].intervals} />;
-			let nameDiv = <div className="name-listing" key={i} style={{borderLeftColor: users[i].color}}>{users[i].id}</div>;
+			let intervalDiv = <Intervals key={i} color={users[i].color} intervals={users[i].intervals}/>;
+			let nameDiv = <div className="name-listing" key={i} style={{ borderLeftColor: users[i].color }}>{users[i].id}</div>;
 
 			// We want to put "others" at bottom of list
-			if (users[i].id === "Other") {
+			if (users[i].id === 'Other') {
 				others.push(nameDiv);
 				otherIntervals.push(intervalDiv);
 				continue;
@@ -34,18 +34,33 @@ function Day(props) {
 	names.push(...others);
 	intervals.push(...otherIntervals);
 
+	let markers = [];
+	const numMarkers = 12;
+	markers.push(<span className="label" style={{ left: '-0.9em' }}>0:00</span>);
+	for (let i = 1; i < numMarkers; i++) {
+		const left = (i * 100 / numMarkers);
+		markers.push(
+			<>
+				<span className="label" style={{ left: `calc(-1.25em + ${left}%)` }}>{i * 2}:00</span>
+				<div className={i % 2 === 0 ? 'marker bold' : 'marker'} style={{ left: left + '%' }} />
+			</>
+		);
+	}
+	markers.push(<span className="label" style={{ left: 'calc(-1.25em + 100%)' }}>24:00</span>);
+
+
 	return (
 		<Card className="day" elevation={4}>
 			<CardContent>
 				<Typography className="date" variant="h6">{props.date}</Typography>
 				<div className="content">
-					<div className="names" style={{height: (names.length * 1.75) + "em"}}>
+					<div className="names" style={{ height: (names.length * 1.75) + 'em' }}>
 						{names}
 					</div>
 					<div className="bar-graphs">
 						{intervals}
+						{markers}
 					</div>
-					{/*<div className="label" />*/}
 				</div>
 			</CardContent>
 		</Card>
