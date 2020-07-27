6e16f2fd0090fb04c1c8d4c69889531e37d79e18
PatrickUbelhor <pubelhor@gmail.com>
2020-07-27 17:13:02 -0500

    Added drawer for mobile navigation

--- react/src/components/Header.js
+++ react/src/components/Header.js
@@ -1,15 +1,17 @@
 import '../css/Header.css';
 import React from 'react';
 import {
-	NavLink,
+	NavLink
 } from 'react-router-dom';
 import {
 	AppBar,
+	Drawer,
 	Icon,
 	IconButton,
 	Toolbar,
 	Tooltip,
-	Typography
+	Typography,
+	useMediaQuery
 } from '@material-ui/core';
 
 
@@ -24,36 +26,88 @@ function invertTheme() {
 }
 
 
-function Header(props) {
-	return (
-		<AppBar id="appBar" position="sticky">
-			<Toolbar>
-				<Typography id="home" variant="h5" color="inherit">Voice Tracker</Typography>
-				<ul className="header-nav">
-					<li className="header-nav-item">
-						<NavLink exact to="/" className="header-nav-link" activeClassName="header-nav-link-active">
-							<span className="header-nav-link-text">DAYS</span>
-						</NavLink>
-					</li>
-					<li className="header-nav-item">
-						<NavLink to="/histograms" className="header-nav-link" activeClassName="header-nav-link-active">
-							<span className="header-nav-link-text">HISTOGRAMS</span>
-						</NavLink>
-					</li>
-					<li className="header-nav-item">
-						<NavLink to="/analytics" className="header-nav-link" activeClassName="header-nav-link-active">
-							<span className="header-nav-link-text">ANALYTICS</span>
-						</NavLink>
-					</li>
-				</ul>
+const title = <Typography id="home" variant="h5" color="inherit">Voice Tracker</Typography>;
+const themeButton = (
+	<Tooltip title="Toggle light/dark mode">
+		<IconButton className="lightModeButton" onClick={invertTheme}>
+			<Icon color="inherit">cloud</Icon>
+		</IconButton>
+	</Tooltip>
+);
 
-				<Tooltip title="Toggle light/dark mode">
-					<IconButton className="lightModeButton" onClick={invertTheme}>
-						<Icon color="inherit" >cloud</Icon>
-					</IconButton>
-				</Tooltip>
-			</Toolbar>
-		</AppBar>
+
+function Header(props) {
+
+	const [open, setOpen] = React.useState();
+	const toggleDrawer = (isOpen) => () => {
+		setOpen(isOpen);
+	}
+
+	const desktop = (
+		<>
+			{title}
+			<ul className="header-nav">
+				<li className="header-nav-item">
+					<NavLink exact to="/" className="header-nav-link" activeClassName="header-nav-link-active">
+						<span className="header-nav-link-text">DAYS</span>
+					</NavLink>
+				</li>
+				<li className="header-nav-item">
+					<NavLink to="/histograms" className="header-nav-link" activeClassName="header-nav-link-active">
+						<span className="header-nav-link-text">HISTOGRAMS</span>
+					</NavLink>
+				</li>
+				<li className="header-nav-item">
+					<NavLink to="/analytics" className="header-nav-link" activeClassName="header-nav-link-active">
+						<span className="header-nav-link-text">ANALYTICS</span>
+					</NavLink>
+				</li>
+			</ul>
+			{themeButton}
+		</>
+	);
+
+	const mobile = (
+		<>
+			<IconButton className="menu-button" onClick={toggleDrawer(true)}>
+				<Icon color="inherit">menu</Icon>
+			</IconButton>
+			{title}
+			{themeButton}
+		</>
+	);
+
+	const drawer = (
+		<Drawer className="drawer" anchor="left" open={open} onClose={toggleDrawer(false)}>
+			<div className="drawer-content">
+				{title}
+				<div>v{process.env.REACT_APP_VERSION}</div>
+				<div className="drawer-divider" />
+				<NavLink exact to="/" className="header-nav-link" activeClassName="header-nav-link-active">
+					<span className="header-nav-link-text">DAYS</span>
+				</NavLink>
+				<NavLink to="/histograms" className="header-nav-link" activeClassName="header-nav-link-active">
+					<span className="header-nav-link-text">HISTOGRAMS</span>
+				</NavLink>
+				<NavLink to="/analytics" className="header-nav-link" activeClassName="header-nav-link-active">
+					<span className="header-nav-link-text">ANALYTICS</span>
+				</NavLink>
+			</div>
+		</Drawer>
+	);
+
+	const isDesktop = useMediaQuery('(min-width: 720px)');
+	console.log(`Is large screen: ${isDesktop}`);
+
+	return (
+		<>
+			<AppBar id="appBar" position="sticky">
+				<Toolbar>
+					{isDesktop ? desktop : mobile}
+				</Toolbar>
+			</AppBar>
+			{isDesktop ? null : drawer}
+		</>
 	);
 }
 
