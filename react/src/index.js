ba98462ae78532f0c6e2f93cc9d3eb848ea5e58c
PatrickUbelhor <pubelhor@gmail.com>
2020-08-05 17:45:40 -0500

    Fixed white flash bug

--- react/src/index.js
+++ react/src/index.js
@@ -5,6 +5,11 @@ import App from './components/App';
 const theme = localStorage.getItem('theme');
 document.body.classList.add(theme ? theme : 'dark');
 
+// Prevents white flash when initializing dark theme
+setTimeout(() => {
+	document.body.classList.add('body-transition');
+}, 100);
+
 // Render the component
 ReactDOM.render(
 	<App/>,
