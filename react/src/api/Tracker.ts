c0d09a187aec2103c5a13e9b8785cff0835b1235
PatrickUbelhor <pubelhor@gmail.com>
2020-11-21 12:46:16 -0600

    Added typing to Tracker

--- react/src/api/Tracker.ts
+++ react/src/api/Tracker.ts
@@ -1,4 +1,7 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
+import { DayResponse, HistogramResponse, StatsResponse } from '../model/Responses';
+
+type TrackerResponse<T> = Promise<AxiosResponse<T>>;
 
 class Tracker {
 
@@ -9,23 +12,23 @@ class Tracker {
 	});
 
 
-	getUsers = () => {
+	getUsers = (): TrackerResponse<string[]> => {
 		console.log(process.env.REACT_APP_SERVER_URL);
 		return this.tracker.get('/users');
 	}
 
 
-	getDays = (newestDay, oldestDay) => {
+	getDays = (newestDay: number, oldestDay: number): TrackerResponse<DayResponse[]> => {
 		return this.tracker.get(`?newestDay=${newestDay}&oldestDay=${oldestDay}`);
 	}
 
 
-	getHistograms = (numDays, minActiveDays) => {
+	getHistograms = (numDays: number, minActiveDays: number): TrackerResponse<HistogramResponse[]> => {
 		return this.tracker.get(`/histogram?numDays=${numDays}&minActiveDays=${minActiveDays}`);
 	}
 
 
-	getAnalytics = (numDays, username) => {
+	getAnalytics = (numDays: number, username: string): TrackerResponse<StatsResponse[]> => {
 		return this.tracker.get(`analytics?numDays=${numDays}&username=${username}`)
 	}
 
