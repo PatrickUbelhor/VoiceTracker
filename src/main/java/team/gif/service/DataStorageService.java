7e23727c8ee44d87210b4342754c21e0ba5d21e7
Patrick Ubelhor <pubelhor@gmail.com>
2021-02-15 16:29:28 -0600

    Fixed calculation of analytics

--- src/main/java/team/gif/service/DataStorageService.java
+++ src/main/java/team/gif/service/DataStorageService.java
@@ -93,37 +93,21 @@ public class DataStorageService {
 		numDays = Math.min(numDays, this.days.size());
 		List<Day> days = this.days.subList(1, numDays + 1);
 		for (Day day : days) {
-			HashMap<String, HashMap<String, Integer[]>> schedulesByChannel = new HashMap<>();
-			
-			// Get all users' schedules as arrays
-			for (Channel channel : day.getChannels()) {
-				schedulesByChannel.putIfAbsent(channel.getId(), new HashMap<>());
-				HashMap<String, Integer[]> schedules = schedulesByChannel.get(channel.getId());
-				
-				for (User user : channel.getUsers()) {
-					Integer[] schedule = convertScheduleToArray(user.getIntervals());
-					schedules.put(user.getId(), schedule);
-				}
-			}
+			HashMap<String, HashMap<String, Integer[]>> schedulesByChannel = getSchedulesAsArraysByChannel(day);
 			
 			// Calculate aggregate daily time for users
 			HashMap<String, Integer[]> aggregateSchedules = new HashMap<>();
 			for (String channelId : schedulesByChannel.keySet()) {
 				for (String userId : schedulesByChannel.get(channelId).keySet()) {
 					aggregateSchedules.putIfAbsent(userId, createEmptySchedule());
-					
-					Integer[] aggregateSchedule = aggregateSchedules.get(userId);
-					for (int min = 0; min < 1440; min++) {
-						// Bitwise OR. This forces aggregate value to be either 0 or 1.
-						// Otherwise if we just added, a user could log into two channels in different guilds simultaneously,
-						// leading to an aggregate sum greater than 1440. This would mess up the stats calculation.
-						aggregateSchedule[min] = aggregateSchedule[min] | schedulesByChannel.get(channelId).get(userId)[min];
-					}
+					addToAggregate(aggregateSchedules.get(userId), schedulesByChannel.get(channelId).get(userId)); // TODO: Make this more concise?
 				}
 			}
 			
+			// Add this day's aggregate time to total time
 			for (String userId : aggregateSchedules.keySet()) {
-				time.put(userId, sumSchedule(aggregateSchedules.get(userId)));
+				int subTime = sumSchedule(aggregateSchedules.get(userId));
+				time.put(userId, subTime + time.getOrDefault(userId, 0));
 			}
 			
 			// Calculate aggregate joint time
@@ -138,24 +122,88 @@ public class DataStorageService {
 						continue;
 					}
 					
-					Integer[] targetSchedule = schedulesByUser.get(userId);
 					Integer[] jointSchedule = jointSchedules.get(userId);
+					Integer[] targetSchedule = schedulesByUser.get(userId);
 					Integer[] originSchedule = schedulesByUser.get(username);
-					for (int min = 0; min < 1440; min++) {
-						// Bitwise logic same as above for aggregateOriginSchedule.
-						// Two users could theoretically each be in two different channels together in two guilds.
-						jointSchedule[min] = jointSchedule[min] | (targetSchedule[min] & originSchedule[min]);
-					}
+					addToJointAggregate(jointSchedule, targetSchedule, originSchedule);
 				}
 			}
 			
 			// Sum minutes in all joint schedules
 			for (String userId : jointSchedules.keySet()) {
-				jointTime.put(userId, sumSchedule(jointSchedules.get(userId)));
+				int subJointTime = sumSchedule(jointSchedules.get(userId));
+				jointTime.put(userId, subJointTime + jointTime.getOrDefault(userId, 0));
 			}
 		}
 		
 		// Compute dependent statistics for origin user
+		return computeStats(time, jointTime, numDays, username);
+	}
+	
+	
+	private HashMap<String, HashMap<String, Integer[]>> getSchedulesAsArraysByChannel(Day day) {
+		HashMap<String, HashMap<String, Integer[]>> schedulesByChannel = new HashMap<>();
+		
+		// Get all users' schedules as arrays
+		for (Channel channel : day.getChannels()) {
+			schedulesByChannel.putIfAbsent(channel.getId(), new HashMap<>());
+			HashMap<String, Integer[]> schedules = schedulesByChannel.get(channel.getId());
+			
+			for (User user : channel.getUsers()) {
+				Integer[] schedule = convertScheduleToArray(user.getIntervals());
+				schedules.put(user.getId(), schedule);
+			}
+		}
+		
+		return schedulesByChannel;
+	}
+	
+	
+	private Integer[] convertScheduleToArray(List<Interval> schedule) {
+		Integer[] result = createEmptySchedule();
+		
+		for (Interval interval : schedule) {
+			Arrays.fill(result, interval.getStart(), interval.getEnd(), 1);
+		}
+		
+		return result;
+	}
+	
+	
+	private void addToAggregate(Integer[] accumulator, Integer[] next) {
+		for (int min = 0; min < 1440; min++) {
+			// Bitwise OR. This forces aggregate value to be either 0 or 1.
+			// Otherwise if we just added, a user could log into two channels in different guilds simultaneously,
+			// leading to an aggregate sum greater than 1440. This would mess up the stats calculation.
+			accumulator[min] = accumulator[min] | next[min];
+		}
+	}
+	
+	
+	private void addToJointAggregate(Integer[] accumulator, Integer[] leftSchedule, Integer[] rightSchedule) {
+		for (int min = 0; min < 1440; min++) {
+			// Bitwise logic same as above for aggregateOriginSchedule.
+			// Two users could theoretically each be in two different channels together in two guilds.
+			accumulator[min] = accumulator[min] | (leftSchedule[min] & rightSchedule[min]);
+		}
+	}
+	
+	
+	private Integer[] createEmptySchedule() {
+		Integer[] schedule = new Integer[1440];
+		Arrays.fill(schedule, 0);
+		return schedule;
+	}
+	
+	
+	private int sumSchedule(Integer[] schedule) {
+		return Arrays.stream(schedule)
+				.reduce(Integer::sum)
+				.get();
+	}
+	
+	
+	private LinkedList<Stats> computeStats(HashMap<String, Integer> time, HashMap<String, Integer> jointTime, int numDays, String username) {
 		final int totalTime = 1440 * numDays;
 		double probOrigin = ((double) time.get(username)) / ((double) totalTime);
 		LinkedList<Stats> stats = new LinkedList<>();
@@ -181,31 +229,6 @@ public class DataStorageService {
 		}
 		
 		return stats;
-	}
-	
-	
-	private Integer[] convertScheduleToArray(List<Interval> schedule) {
-		Integer[] result = createEmptySchedule();
-		
-		for (Interval interval : schedule) {
-			Arrays.fill(result, interval.getStart(), interval.getEnd(), 1);
-		}
-		
-		return result;
-	}
-	
-	
-	private Integer[] createEmptySchedule() {
-		Integer[] schedule = new Integer[1440];
-		Arrays.fill(schedule, 0);
-		return schedule;
-	}
-	
-	
-	private int sumSchedule(Integer[] schedule) {
-		return Arrays.stream(schedule)
-				.reduce(Integer::sum)
-				.get();
 	}
 	
 	
