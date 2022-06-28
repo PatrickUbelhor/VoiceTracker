742b11f44e437ddbf4355253ce32c5c49ba4cf09
Patrick Ubelhor <pubelhor@gmail.com>
2022-06-27 23:01:05 -0500

    Turned ErrorSnackbar into a more pure React function

--- react/src/components/ErrorSnackbar.tsx
+++ react/src/components/ErrorSnackbar.tsx
@@ -1,6 +1,6 @@
 import './ErrorSnackbar.css';
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { AppState } from '../model/States';
 import { clearError } from '../state/Actions';
 import CloseIcon from '@mui/icons-material/Close';
@@ -8,67 +8,58 @@ import ErrorIcon from '@mui/icons-material/Error';
 import IconButton from '@mui/material/IconButton';
 import Snackbar from '@mui/material/Snackbar';
 
-interface IProps {
-	message: string;
-	resetMessage: () => void;
-}
 
-const select = (state: AppState) => ({
-	message: state.errorMessage
-});
+function ErrorSnackbar() {
 
-const mapDispatchToProps = (dispatch) => ({
-	resetMessage: () => dispatch(clearError())
-});
-
-
-function ConnectedErrorSnackbar(props: IProps) {
+	const message: string = useSelector<AppState, string>(state => state.errorMessage);
+	const dispatch = useDispatch();
+	const resetMessage = useCallback(
+		() => dispatch(clearError()),
+		[dispatch]
+	);
 
 	const handleClose = (event, reason: string) => {
 		if (reason === 'clickaway') {
 			return;
 		}
 
-		this.props.resetMessage();
+		resetMessage();
 	};
 
-	const handleButtonClose = (event) => {
-		this.props.resetMessage();
+	const handleButtonClose = () => {
+		resetMessage();
 	};
 
 	return (
-		<div>
-			<Snackbar
-				anchorOrigin={{
-					vertical: 'top',
-					horizontal: 'center',
-				}}
-				open={props.message !== null}
-				autoHideDuration={4000}
-				onClose={handleClose}
-				ContentProps={{
-					'aria-describedby': 'message',
-				}}
-				message={
-					<span id="message">
-						<ErrorIcon id="errorIcon" />
-						{props.message}
-					</span>
-				}
-				action={
-					<IconButton
-						key="close"
-						aria-label="close"
-						color="inherit"
-						onClick={handleButtonClose}
-					>
-						<CloseIcon />
-					</IconButton>
-				}
-			/>
-		</div>
+		<Snackbar
+			anchorOrigin={{
+				vertical: 'top',
+				horizontal: 'center',
+			}}
+			open={message !== null}
+			autoHideDuration={4000}
+			onClose={handleClose}
+			ContentProps={{
+				'aria-describedby': 'message',
+			}}
+			message={
+				<span id="message">
+					<ErrorIcon id="errorIcon" />
+					{message}
+				</span>
+			}
+			action={
+				<IconButton
+					key="close"
+					aria-label="close"
+					color="inherit"
+					onClick={handleButtonClose}
+				>
+					<CloseIcon />
+				</IconButton>
+			}
+		/>
 	);
 }
 
-const ErrorSnackbar = connect(select, mapDispatchToProps)(ConnectedErrorSnackbar);
 export default ErrorSnackbar;
