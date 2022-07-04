b53844f0afa8f9c38b1c7f35a901f0c8114f4771
PatrickUbelhor <pubelhor@gmail.com>
2022-07-04 14:53:59 -0500

    Converted App component to React function

--- react/src/components/App.tsx
+++ react/src/components/App.tsx
@@ -1,8 +1,7 @@
 import './App.css';
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
-import { AppState } from '../model/States';
+import { useAppDispatch, useAppSelector } from '../state/Hooks';
 import AnalyticsPage from './analytics-page/AnalyticsPage';
 import DayList from './day-page/DayList';
 import ErrorSnackbar from './ErrorSnackbar';
@@ -13,63 +12,44 @@ import {
 	Route,
 	Switch
 } from 'react-router-dom';
-import { getMuiTheme } from '../model/Themes';
+import { getMuiTheme, Themes } from '../model/Themes';
 import { initApp } from '../state/Effects';
-import { setError } from '../state/Actions';
 
-interface IProps {
-	theme: string;
-	initApp: () => void;
-	setErrorMessage: (message: string) => void;
+
+export default function App() {
+	const dispatch = useAppDispatch();
+	const theme: Themes = useAppSelector(state => state.theme);
+
+	useEffect(() => {
+		dispatch(initApp());
+	});
+
+
+	return (
+		<ThemeProvider theme={getMuiTheme(theme)}>
+			<div className="wrapper">
+				<Router>
+					<Header/>
+
+					<Switch>
+						<Route path="/analytics">
+							<AnalyticsPage/>
+						</Route>
+						<Route path="/histograms">
+							<HistogramList />
+						</Route>
+						<Route path="/">
+							<DayList />
+						</Route>
+					</Switch>
+
+					<ErrorSnackbar/>
+				</Router>
+			</div>
+			<div className="footer">
+				<div className="copyright">© Patrick Ubelhor 2022</div>
+				<div className="version">v{process.env.REACT_APP_VERSION}</div>
+			</div>
+		</ThemeProvider>
+	);
 }
-
-const select = (state: AppState) => ({
-	theme: state.theme
-});
-
-
-const mapDispatchToProps = (dispatch) => ({
-	initApp: () => dispatch(initApp()),
-	setErrorMessage: (message: string) => dispatch(setError(message))
-});
-
-
-class ConnectedApp extends React.Component<IProps, any> {
-
-	componentDidMount() {
-		this.props.initApp();
-	}
-
-	render() {
-		return (
-			<ThemeProvider theme={getMuiTheme(this.props.theme)}>
-				<div className="wrapper">
-					<Router>
-						<Header/>
-
-						<Switch>
-							<Route path="/analytics">
-								<AnalyticsPage/>
-							</Route>
-							<Route path="/histograms">
-								<HistogramList />
-							</Route>
-							<Route path="/">
-								<DayList />
-							</Route>
-						</Switch>
-
-						<ErrorSnackbar/>
-					</Router>
-				</div>
-				<div className="footer">
-					<div className="copyright">© Patrick Ubelhor 2022</div>
-					<div className="version">v{process.env.REACT_APP_VERSION}</div>
-				</div>
-			</ThemeProvider>
-		);
-	}
-}
-
-const App = connect(select, mapDispatchToProps)(ConnectedApp);
-export default App;
