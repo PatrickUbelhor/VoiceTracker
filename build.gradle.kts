bf2f339ddcb16ddf31698756b9273c278a2a3598
Patrick Ubelhor <pubelhor@gmail.com>
2025-07-12 21:44:05 -0400

    Updated dependencies and java to v21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,17 @@
 
 plugins {
 	java
-	id("org.springframework.boot") version("3.0.1")
+	id("org.springframework.boot") version("3.5.3")
 }
 
 apply(plugin = "io.spring.dependency-management")
 
-group = "team.gif"
-version = "3.0.0"
+group = "com.patrickubelhor"
+version = "3.0.1"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
-	targetCompatibility = JavaVersion.VERSION_17
+	sourceCompatibility = JavaVersion.VERSION_21
+	targetCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
@@ -24,6 +24,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
-	implementation(group = "org.apache.logging.log4j", name = "log4j-core", version = "2.19.0")
+	implementation(group = "org.apache.logging.log4j", name = "log4j-core", version = "2.25.1")
 	runtimeOnly(group = "org.postgresql", name = "postgresql")
 }
